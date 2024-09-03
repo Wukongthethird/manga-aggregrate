eba018e7632fdc3a14dbf73932131c410c48bdea
Simon Zhang <simonzhang@Simons-MacBook-Air.local>
2024-09-03 00:49:24 -0700

    searchmangadexmanga return data and interface. get mangadexfee returns base off of newest chapters

--- Server/src/API/mangadexAPI.ts
+++ Server/src/API/mangadexAPI.ts
@@ -3,6 +3,15 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
+interface searchMangadexMangaInterface {
+  id: string;
+  title: string;
+  link: string;
+  altTitles: string[];
+
+  //author and artist later if wanted
+}
+
 export default class mangadexAPI {
   static BASE_URL = process.env.MANGADEX_BASE_URL;
 
@@ -26,7 +35,9 @@ export default class mangadexAPI {
         headers,
       });
     } catch (error: any) {
-      console.error("api ERROR", error);
+      //   console.error("api ERROR", error);
+      //mangadex api drilled error
+      console.log(error?.response?.data);
     }
   }
 
@@ -52,6 +63,7 @@ export default class mangadexAPI {
       const { access_token, refresh_token } = res.data;
       this.access_token = access_token;
       this.refresh_token = refresh_token;
+      console.log(this.access_token);
     } catch (error: any) {
       console.log(error.data.errors);
     }
@@ -87,7 +99,7 @@ export default class mangadexAPI {
       this.access_token = res?.data?.access_token;
       this.refresh_token = res?.data.refresh_token;
     } catch (error: any) {
-      console.log(error);
+      console.log(error.data.errors);
     }
   }
 
@@ -96,20 +108,55 @@ export default class mangadexAPI {
     return res;
   }
 
-  static async searchMangadexManga(title: string) {
-    const res = await this.request(`manga`, { title });
-    return res;
+  static async searchMangadexManga(
+    title: string
+  ): Promise<searchMangadexMangaInterface[]> {
+    const res = await this.request(`manga`, { title }).then(
+      (res) => res?.data?.data
+    );
+
+    const response = [];
+
+    if (res.length) {
+      for (let i = 0; i < res.length; i++) {
+        const id = res[i]?.id as string;
+        const title = res[i]?.attributes?.title.en as string;
+        const link = `https://mangadex.org/title/${id}` as string;
+
+        // const altTitles = res[i]?.attributes?.altTitles?.filter(title => title.en);
+        const altTitles: string[] = [];
+        for (let j = 0; j < res[i]?.attributes?.altTitles.length; j++) {
+          if ("en" in res[i]?.attributes?.altTitles[j]) {
+            altTitles.push(res[i]?.attributes?.altTitles[j].en);
+          }
+        }
+        response.push({ id, title, link, altTitles });
+      }
+    }
+
+    return response;
   }
 
-  static async getMangadexFeed() {
+  //like this "2024-08-29T23:20:50"
+  static async getMangadexFeed(dateTime: string) {
     // res?.data?.data gives chapter id then with data -> relationships for group and manganame
     const res = await this.request(`user/follows/manga/feed`, {
       translatedLanguage: ["en"],
       order: {
         updatedAt: "desc",
+        readableAt: "desc",
+        publishAt: "desc",
       },
+      publishAt: dateTime,
+      readableAt: dateTime,
+      limit: 20,
     });
 
-    console.log(res?.data?.data[0]);
+    // console.log(res?.data);
+
+    // date time should look like this  2024-08-12T23:20:50
+    // returns newest chapter maybe manga id and time of updates. proba want to compare last check is after last update to ping discord
+    // create return type
+    // console.log(res?.data?.data);
   }
 }
