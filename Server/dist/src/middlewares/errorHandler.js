2c97d9aa4587516350fcb33a4eccfcff760cd69a
Simon Zhang <simonzhang@Simons-MacBook-Air.local>
2024-09-03 18:50:12 -0700

    remove dist

--- Server/dist/src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    return res.status(500).json({ err });
-};
-exports.default = errorHandler;
